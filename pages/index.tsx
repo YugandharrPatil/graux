ac34beb08b5bd284eb76790c841873fbefa6411a
Yugandhar Patil <yugandhar.patil@learner.manipal.edu>
2023-03-14 00:34:35 +0530

    bg images added to more sectins

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,31 +25,34 @@ export default function Home() {
 			{/* stats */}
 
 			<main className={styles.stats}>
-				<div className="info mx-3">
-					<h3 className="mt-3">Transaction Speed (TPS)</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit deserunt repellat sequi sapiente fugit labore temporibus rerum. Sunt,
-						dolorum ullam? Sint ab repudiandae facilis, libero modi quas iure suscipit beatae?
-					</p>
+				<div>
+					<h1>Transaction Speed (TPS)</h1>
+					<h5 className="text-center">100,000+</h5>
 				</div>
-				<div className="info mx-3">
-					<h3 className="mt-3">Total Transactions Completed</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit deserunt repellat sequi sapiente fugit labore temporibus rerum. Sunt,
-						dolorum ullam? Sint ab repudiandae facilis, libero modi quas iure suscipit beatae?
-					</p>
+				<div>
+					<h1>Total Transactions Completed</h1>
+					<h5 className="text-center">1,000,000+</h5>
 				</div>
-				<div className="info mx-3">
-					<h3 className="mt-3">Validators & Nodes</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit deserunt repellat sequi sapiente fugit labore temporibus rerum. Sunt,
-						dolorum ullam? Sint ab repudiandae facilis, libero modi quas iure suscipit beatae?
-					</p>
+				<div>
+					<h1>Validators & Nodes</h1>
+					<h5 className="text-center">1000+</h5>
 				</div>
 			</main>
 
+			{/* What is Graux */}
+
 			<main className={styles.what}>
 				<h2>What is Graux?</h2>
+				<p className="pt-5">
+					Graux is an interoperability protocol. It will help in providing faster and user friendly system where you can build Dapps on multi-chain
+					one-click user experience accessing token De-Fi, gamify and governance. You can convert and use Graux as a hybrid blockchain and use
+					different multichains to do a transaction in private entity
+				</p>
+				<p className="pt-5">
+					Graux is an interoperability protocol. It will help in providing faster and user friendly system where you can build Dapps on multi-chain
+					one-click user experience accessing token De-Fi, gamify and governance. You can convert and use Graux as a hybrid blockchain and use
+					different multichains to do a transaction in private entity
+				</p>
 				<p className="pt-5">
 					Graux is an interoperability protocol. It will help in providing faster and user friendly system where you can build Dapps on multi-chain
 					one-click user experience accessing token De-Fi, gamify and governance. You can convert and use Graux as a hybrid blockchain and use
